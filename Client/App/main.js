d2d9dcf559712adc709b499dff62eba56dfc8c10
florimm <emri.mbiemri@gmail.com>
2014-02-21 20:05:29 +0100

    Init

---
+++ Client/App/main.js
@@ -0,0 +1,38 @@
+﻿requirejs.config({
+    paths: {
+        'text': '../Scripts/text',
+        'durandal': '../Scripts/durandal',
+        'plugins': '../Scripts/durandal/plugins',
+        'transitions': '../Scripts/durandal/transitions',
+        'async':'../Scripts/async'
+    }
+});
+
+define('jquery', function() { return jQuery; });
+define('knockout', ko);
+define('gmaps', ['async!http://maps.google.com/maps/api/js?v=3&sensor=false'],function () {
+    return window.google.maps;
+});
+
+define(['durandal/system', 'durandal/app', 'durandal/viewLocator'],  function (system, app, viewLocator) {
+    //>>excludeStart("build", true);
+    system.debug(true);
+    //>>excludeEnd("build");
+
+    app.title = 'Durandal Starter Kit';
+
+    app.configurePlugins({
+        router: true,
+        dialog: true,
+        widget: true
+    });
+
+    app.start().then(function() {
+        //Replace 'viewmodels' in the moduleId with 'views' to locate the view.
+        //Look for partial views in a 'views' folder in the root.
+        viewLocator.useConvention();
+
+        //Show the app by setting the root view model for our application with a transition.
+        app.setRoot('viewmodels/shell', 'entrance');
+    });
+});
---
+++ Client/App/viewmodels/map.js
@@ -0,0 +1,46 @@
+﻿define(['plugins/http', 'durandal/app', 'knockout','gmaps'], function (http, app, ko,gmaps) {
+    //Note: This module exports an object.
+    //That means that every module that "requires" it will get the same object instance.
+    //If you wish to be able to create multiple instances, instead export a function.
+    //See the "welcome" module for an example of function export.
+
+    return {
+        displayName: 'Maps',
+        images: ko.observableArray([]),
+        compositionComplete:function(){
+            alert('compose');
+            var mapDiv = document.getElementById('mapcanvas');
+            var map = new gmaps.Map(mapDiv, {
+                center: new gmaps.LatLng(37.4419, -122.1419),
+                zoom: 13,
+                mapTypeId: gmaps.MapTypeId.ROADMAP,
+                navigationControl: true,
+                navigationControlOptions: {
+                    style: gmaps.NavigationControlStyle.SMALL
+                }
+            });
+            var marker = new gmaps.Marker({
+                position: map.getCenter(),
+                map: map,
+                title: 'Click to zoom'
+            });
+            gmaps.event.addListener(marker, 'click', function () {
+                map.setZoom(8);
+                map.setCenter(marker.getPosition());
+            });
+        },
+        activate: function () {
+            alert('actovate');
+        },
+        select: function (item) {
+            //the app model allows easy display of modal dialogs by passing a view model
+            //views are usually located by convention, but you an specify it as well with viewUrl
+            item.viewUrl = 'views/detail';
+            app.showDialog(item);
+        },
+        canDeactivate: function () {
+            //the router's activator calls this function to see if it can leave the screen
+            return app.showMessage('Are you sure you want to leave this page?', 'Navigate', ['Yes', 'No']);
+        }
+    };
+});
