8f7e0f5bd76b1d70c8bc8c7ccdd5bcab7bb1e91a
florimm <emri.mbiemri@gmail.com>
2014-02-24 23:29:56 +0100

    Added event when closing POLYGON.

--- Client/App/viewmodels/map.js
+++ Client/App/viewmodels/map.js
@@ -1,13 +1,14 @@
-﻿define(['plugins/http', 'durandal/app', 'knockout','gmaps'], function (http, app, ko,gmaps) {
+﻿define(['plugins/http', 'durandal/app', 'knockout', 'gmaps', 'durandal/system'], function (http, app, ko, gmaps,system) {
     //Note: This module exports an object.
     //That means that every module that "requires" it will get the same object instance.
     //If you wish to be able to create multiple instances, instead export a function.
     //See the "welcome" module for an example of function export.
-
+    var iw = new gmaps.InfoWindow();
     return {
         displayName: 'Maps',
         markers: ko.observableArray([]),
-        map:null,
+        map: null,
+        
         placeMarker: function(location) {
             var marker = new gmaps.Marker({
                 position: location,
@@ -20,21 +21,22 @@
             var that = this;
             var mapDiv = document.getElementById('mapcanvas');
             var drawingManager = new gmaps.drawing.DrawingManager({
-                drawingMode: gmaps.drawing.OverlayType.MARKER,
+                drawingMode: gmaps.drawing.OverlayType.POLYGON,
                 drawingControl: true,
                 drawingControlOptions: {
                     position: gmaps.ControlPosition.TOP_CENTER,
                     drawingModes: [
-                        gmaps.drawing.OverlayType.MARKER,
-                        gmaps.drawing.OverlayType.POLYGON,
-                        gmaps.drawing.OverlayType.POLYLINE
+                        gmaps.drawing.OverlayType.POLYGON
                     ]
+                },
+                polygonOptions: {
+                    editable: true
                 }
             });
 
             that.map = new gmaps.Map(mapDiv, {
                 center: new gmaps.LatLng(37.4419, -122.1419),
-                zoom: 13,
+                zoom: 14,
                 mapTypeId: gmaps.MapTypeId.ROADMAP,
                 navigationControl: true,
                 navigationControlOptions: {
@@ -42,10 +44,31 @@
                 }
             });
             drawingManager.setMap(that.map);
+            gmaps.event.addListener(drawingManager, "overlaycomplete", function (event) {
+                system.log(event);
+                //if (event.type == gmaps.drawing.OverlayType.POLYGON) {
+                //    drawingManager.setDrawingMode(null);
+                //}
+                var newShape = event.overlay;
+                newShape.type = event.type;
+
+            });
+
+            gmaps.event.addListener(drawingManager, "overlaycomplete", function (event) {
+                overlayClickListener(event.overlay);
+                system.log(event.overlay.getPath().getArray());
+            });
+
+            function overlayClickListener(overlay) {
+                gmaps.event.addListener(overlay, "mouseup", function (event) {
+                    system.log(overlay.getPath().getArray());
+                });
+            }
             //gmaps.event.addListener(that.map, 'click', function (event) {
             //    that.placeMarker(event.latLng);
             //});
         },
+
         activate: function () {
         },
         select: function (item) {
